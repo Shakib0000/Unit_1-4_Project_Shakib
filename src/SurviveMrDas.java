052b0ade8f42e92376e02e5c2f46eafe9638e1af
Shakib0000 <shakiba4@nycstudents.net>
2023-11-06 13:59:30 -0500

    finished first draft of project

--- src/SurviveMrDas.java
+++ src/SurviveMrDas.java
@@ -3,7 +3,7 @@ public class SurviveMrDas {
     // Instance Variables
     private String name;
     private int dasLocation = 20; // The distance in feet from the player.
-    private int timer = 60; // The amount of time left in minutes.
+    private int timer = 30; // The amount of time left in minutes.
     private int tasksCompleted = 0; // The number of tasks completed.
     private int useInstagramProgress = 0; // Percentage progress of scrolling through Instagram.
     private int watchFortniteProgress = 0; // Percentage progress of watching Fortnite videos.
@@ -21,54 +21,87 @@ public class SurviveMrDas {
         this.name = name;
     }
 
-    // Methods
-
-    public void passTime(int minutes) {
-        timer -= minutes;
-    }
+    // Getter Methods
 
     public int getTimer() {
         return timer;
+    }
+
+    public int getTasksCompleted() {
+        return tasksCompleted;
+    }
+
+    public int getDasLocation() {
+        return dasLocation;
+    }
+
+    public boolean getFinishingTasks() {
+        return finishingTasks;
+    }
+
+    // Methods for Game Mechanics
+
+    public void passTime(int minutes) {
+        timer -= minutes;
     }
 
     public void moveDas(int amount) {
         dasLocation += amount;
     }
 
-    public String fulfillTask(int task) {
-        if (task == 1 & useInstagramProgress != 100) {
+    public String fulfillTask(String task) {
+        if (task.equals("1") && useInstagramProgress != 100) {
             finishingTasks = true;
             useInstagramProgress += 25;
-            return "Your progress on scrolling through Instagram is: " + useInstagramProgress + "%";
+            if (useInstagramProgress == 100) {
+                tasksCompleted += 1;
+            }
+            return "\nYour progress on scrolling through Instagram is: " + useInstagramProgress + "%";
         }
-        else if (task == 1 & useInstagramProgress == 100) {
-            return "You have already finished scrolling through Instagram.";
+        else if (task.equals("1") && useInstagramProgress == 100) {
+            return "\nYou have already finished scrolling through Instagram.";
         }
-        else if (task == 2 & watchFortniteProgress != 100) {
+        else if (task.equals("2") && watchFortniteProgress != 100) {
             finishingTasks = true;
             watchFortniteProgress += 25;
-            return "Your progress on watching Fortnite videos is: " + watchFortniteProgress + "%";
+            if (watchFortniteProgress == 100) {
+                tasksCompleted += 1;
+            }
+            return "\nYour progress on watching Fortnite videos is: " + watchFortniteProgress + "%";
         }
-        else if (task == 2 & useInstagramProgress == 100) {
-            return "You have already finished watching Fortnite videos.";
+        else if (task.equals("2") && watchFortniteProgress == 100) {
+            return "\nYou have already finished watching Fortnite videos.";
         }
-        else if (task == 3 & playAmongUsProgress != 100) {
+        else if (task.equals("3") && playAmongUsProgress != 100) {
             finishingTasks = true;
             playAmongUsProgress += 25;
-            return "Your progress on watching Fortnite videos is: " + playAmongUsProgress + "%";
+            if (playAmongUsProgress == 100) {
+                tasksCompleted += 1;
+            }
+            return "\nYour progress on playing Among Us is: " + playAmongUsProgress + "%";
         }
-        else if (task == 3 & playAmongUsProgress == 100) {
-            return "You have already finished playing Among Us.";
+        else if (task.equals("3") && playAmongUsProgress == 100) {
+            return "\nYou have already finished playing Among Us.";
         }
-        else if (task == 4 & cartwheelProgress != 100) {
+        else if (task.equals("4") && cartwheelProgress != 100) {
             finishingTasks = true;
             cartwheelProgress += 25;
-            return "Your progress on cartwheeling across the room is: " + cartwheelProgress + "%";
+            if (cartwheelProgress == 100) {
+                tasksCompleted += 1;
+            }
+            return "\nYour progress on cartwheeling across the room is: " + cartwheelProgress + "%";
         }
-        else if (task == 4 & cartwheelProgress == 100) {
-            return "You have already finished cartwheeling across the room.";
+        else if (task.equals("4") && cartwheelProgress == 100) {
+            return "\nYou have already finished cartwheeling across the room.";
         }
-        return "There was an error.";
+        else if (task.equals("4") && cartwheelProgress == 100) {
+            return "\nYou have already finished cartwheeling across the room.";
+        }
+        else if (task.equals("5")) {
+            finishingTasks = false;
+            return "\nYou have begun doing extremely boring AP CSA work... keep in mind that you are fighting the urge to sleep.";
+        }
+        return "\nThere was an error.";
     }
 
 
--- src/SurviveMrDasRunner.java
+++ src/SurviveMrDasRunner.java
@@ -8,10 +8,37 @@ public class SurviveMrDasRunner {
     }
 
     public static void main(String[] args) {
-        String name = askQuestion("Welcome to Survive Mr. Das!\nTurn your lights on if you aren't fond of spooks, because what may seem simple and harmless may just bring about your doom.\nTo survive, you must complete four specific tasks before class ends.\nIf you fail to complete the tasks by the end of class or if you are caught by Mr. Das while doing your tasks, you will not be a happy person.\nJust keep that in mind.\n\nAnyways, enter your name: ");
+        String name = askQuestion("Welcome to Survive Mr. Das!\nTurn your lights on if you aren't fond of spooks, because what may seem simple and harmless may just bring about your doom.\nTo survive, you must complete four specific tasks before you end up falling asleep or being caught doing those tasks by Mr. Das.\nIf you are caught asleep or if you are caught by Mr. Das while doing your tasks, you will not be a happy person.\nJust keep that in mind.\n\nAnyways, enter your name: ");
         SurviveMrDas gameManager = new SurviveMrDas(name);
-
-
+        while (gameManager.getTimer() > 0 && gameManager.getTasksCompleted() < 4 && gameManager.getDasLocation() > 0) {
+            String action = askQuestion("\nMr. Das is " + gameManager.getDasLocation() + " feet away and you are " + gameManager.getTimer() + " minutes away from falling asleep.\nWhat do you want to do? Enter one of the following 4 choices:\n\"1\" to scroll through Instagram\n\"2\" to watch Fortnite videos\n\"3\" to play Among Us\n\"4\" to cartwheel across the room\n\"5\" to work on boring, AP CSA work\n");
+            System.out.println(gameManager.fulfillTask(action));
+            gameManager.passTime(1);
+            if (gameManager.getFinishingTasks()) {
+                gameManager.moveDas(-5);
+            }
+            else {
+                if (gameManager.getDasLocation() < 30) {
+                    gameManager.moveDas(5);
+                }
+            }
+        }
+        if (gameManager.getTimer() > 0 && gameManager.getTasksCompleted() == 4 && gameManager.getDasLocation() > 0) {
+            System.out.println("\nYou have managed to stay awake and you were not caught off-task during Mr. Das' class.");
+            System.out.println("Mr. Das: Impressive, " + gameManager.getName() + ". You managed to survive my class. You get to live yet another day. Let's see if you can survive tomorrow as well...\n*You leave AP CSA class in one piece*\nYOU WIN!");
+        }
+        else if (gameManager.getDasLocation() <= 0) {
+            System.out.println("\nMr. Das is looking right down at you. You look up, your face filled with terror.");
+            askQuestion("Mr. Das: " + gameManager.getName() + ", HOW DARE YOU BE OFF-TASK IN MY CLASS!!!!!!!!!!!! YOU ABSOLUTE FOOL. DO YOU HAVE ANY LAST WORDS? ");
+            System.out.println("Mr. Das: OK. I PROBABLY WON'T EVEN BOTHER PUTTING THEM ON YOUR GRAVESTONE. SEE YOU IN THE AFTERLIFE BUDDY! \n*You perish under Mr. Das' wrath*\nGAME OVER!");
+        }
+        else if (gameManager.getTimer() <= 0) {
+            System.out.println("\nYou fall asleep from boredom. You are woken up by Mr. Das' violent footsteps behind you.");
+            askQuestion("Mr. Das: " + gameManager.getName() + ", HOW DARE YOU FALL ASLEEP IN MY CLASS!!!!!!!!!!!! YOU ABSOLUTE FOOL. DO YOU HAVE ANY LAST WORDS? ");
+            System.out.println("Mr. Das: OK. I PROBABLY WON'T EVEN BOTHER PUTTING THEM ON YOUR GRAVESTONE. SEE YOU IN THE AFTERLIFE BUDDY! \n*You perish under Mr. Das' wrath*\nGAME OVER!");
+        }
+        else {
+            System.out.println("If you are seeing this message, somehow the game broke?!?!?!");
+        }
     }
-
 }
