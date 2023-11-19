8ece13f2f12ee611c2e0b14fd7e127af7249b494
Shakib <shakiba4@nycstudents.net>
2023-11-19 18:07:25 -0500

    added helpful comments

--- src/SurviveMrDas.java
+++ src/SurviveMrDas.java
@@ -7,8 +7,8 @@
 public class SurviveMrDas {
     // Creating instance variables to serve as game properties
     private String name; // The name of the player.
-    private int dasLocation; // The distance in feet from the player.
-    private int timer; // The amount of time left in minutes.
+    private int dasLocation; // The distance between Mr. Das and the player in feet.
+    private int timer; // The amount of time left in minutes before the player falls asleep.
     private int tasksCompleted; // The number of tasks completed.
     private int useInstagramProgress; // Percentage progress of scrolling through Instagram.
     private int watchFortniteProgress; // Percentage progress of watching Fortnite videos.
